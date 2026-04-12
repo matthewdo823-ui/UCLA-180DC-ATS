73c5ad3ec4619fba0674b106dc45549d244d57f0
Matthew Do <matthewdo823@gmail.com>
2026-04-11 20:24:22 -0700

    a lil sql but it lwk don't work

---
+++ test.session.sql
@@ -0,0 +1,28 @@
+CREATE TABLE resumes (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    file_url TEXT NOT NULL,
+    review_count INT NOT NULL DEFAULT 0,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE reviews(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    resume_id UUID NOT NULL REFERENCES resumes(id) ON DELETE CASCADE,
+    scores JSONB NOT NULL,
+    notes TEXT,
+    submitted_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+
+UPDATE resumes 
+SET review_count = 0
+where name = 'Matthew Do';
+
+
+SELECT file_url FROM resumes 
+ORDER BY review_count ASC, created_at ASC
+LIMIT 1;
+
+
+/* id, resume id, scores, notes sbumitted_at*/
